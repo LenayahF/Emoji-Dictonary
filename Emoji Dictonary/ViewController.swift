f6caeb5fccb65ac9efda10e2601bee64cca9907e
Lenayah <lfanesie@gmail.com>
2024-02-26 11:05:50 -0500

    Initial Commit

---
+++ Emoji Dictonary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emoji Dictonary
+//
+//  Created by 11k on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
