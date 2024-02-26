63527ac06499cf4da008a10f91cc313a5e20b8c9
Lenayah <lfanesie@gmail.com>
2024-02-26 12:07:25 -0500

    update

---
+++ Emoji Dictonary/emo.swift
@@ -0,0 +1,15 @@
+//
+//  emo.swift
+//  Emoji Dictonary
+//
+//  Created by 11k on 2/26/24.
+//
+
+import Foundation
+
+struct Emoji {
+   var symbol: String
+   var name: String
+   var description: String
+   var usage: String
+}
